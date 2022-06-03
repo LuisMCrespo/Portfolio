1042038bc7cb07bb205f66d3e6e3f66b90e94b01
Crespo <luiscrespo72@gmail.com>
2022-06-03 21:14:31 +0100

    Addded background and header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <body className="body">
+      <header className="header"></header>
+     <p className="text">Ola sou o Luis</p>
+    </body>
+  );
+}
+
+export default App;
